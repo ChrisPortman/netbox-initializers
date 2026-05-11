8f215d78cd3c86979ed0436ef29dffdaa1bac7c7
Chris Portman <cportman@iron.ore>
2026-05-11 11:32:56 +1000

    fixup

--- src/netbox_initializers/initializers/base.py
+++ src/netbox_initializers/initializers/base.py
@@ -84,7 +84,7 @@ class BaseInitializer:
             restricted_cts = tag.object_types.all()
             if restricted_cts and ct not in restricted_cts:
                 raise Exception(
-                    f"⚠️ Tag {tag} cannot be applied to {entity}'s content type {ct.natural_key()}"
+                    f"⚠️ Tag {tag} cannot be applied to {entity}'s content type {ct.natural_key()}, applicablt content types for tag: {restricted_cts}"
                 )
 
             entity.tags.add(tag)
