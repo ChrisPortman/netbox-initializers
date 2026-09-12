32997d4cb582807a1f18c443fa7983710f371787
Chris Portman <cportman@iron.ore>
2026-09-12 18:00:43 +1000

    f

--- src/netbox_initializers/initializers/base.py
+++ src/netbox_initializers/initializers/base.py
@@ -143,7 +143,7 @@ class BaseInitializer:
         save = False
 
         for mac in mac_addresses:
-            if entity.mac_addresses.filter(mac_addresses=mac).first():
+            if entity.mac_addresses.filter(mac_address=mac).first():
                 continue
 
             mac_address = MACAddress.objects.create(mac_address=mac, description=f"{entity} MAC Address")
